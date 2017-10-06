57a3375182ecdabca5f86fd2da238d475522fe8f
Pelmanable <31858728+Pelmanable@users.noreply.github.com>
2017-10-06 19:23:07 +0300

    Создание проекта с помощью Maven, попытка 2

---
+++ HelloMaven/src/main/java/com/companyname/integral/App.java
@@ -0,0 +1,14 @@
+package com.companyname.integral;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Integral!" );
+        System.out.println("This is MAVEN project!");
+    }
+}
