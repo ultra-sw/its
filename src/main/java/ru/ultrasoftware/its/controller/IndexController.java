aaa083337ca6a0cf4742f993532dbf07053d6a91
Pelmanable <31858728+Pelmanable@users.noreply.github.com>
2017-10-25 20:55:56 +0300

    Add files via upload

--- src/main/java/ru/ultrasoftware/its/controller/IndexController.java
+++ src/main/java/ru/ultrasoftware/its/controller/IndexController.java
@@ -1,7 +1,6 @@
 package ru.ultrasoftware.its.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.SpringSecurityMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
