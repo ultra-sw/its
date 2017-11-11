be165aba8f5d71ac7a9a59e57076acdf3dac9896
Alex <alex.alkor@bk.ru>
2017-11-11 13:20:16 +0300

    При авторизации определяется роль пользователя в системе Получаем информацию о сессии пользователя (SessionData) Получаем информацию о всех тикетах (тест) Назначение прав пользователю в зависимости от его роли

--- src/main/java/ru/ultrasoftware/its/security/OtrsAuthenticationProvider.java
+++ src/main/java/ru/ultrasoftware/its/security/OtrsAuthenticationProvider.java
@@ -13,48 +13,75 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import ru.ultrasoftware.its.domain.OtrsSession;
 import ru.ultrasoftware.its.domain.OtrsTickets;
-
+import ru.ultrasoftware.its.domain.OtrsUserInfo;
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Collection;
 
 @Component
 public class OtrsAuthenticationProvider implements AuthenticationProvider {
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
         String password = (String) authentication.getCredentials();
-
+        boolean ROLE_AGENT = true;
         //TODO get session id from otrs
         UriComponents uri = UriComponentsBuilder
                 .fromHttpUrl("http://it.nvrs.net:7777/otrs/nph-genericinterface.pl/Webservice/GenericTicketConnectorREST/Session")
                 .queryParam("UserLogin", username)
                 .queryParam("Password", password)
                 .build();
-        UriComponents tic = UriComponentsBuilder
+        String urlString = uri.toUriString();
+        RestTemplate restTemplate = new RestTemplate();
+        OtrsSession sessionID = restTemplate.postForObject(urlString, null, OtrsSession.class);
+
+        if(sessionID.getSessionId()==null){
+            uri = UriComponentsBuilder
+                    .fromHttpUrl("http://it.nvrs.net:7777/otrs/nph-genericinterface.pl/Webservice/GenericTicketConnectorREST/Session")
+                    .queryParam("CustomerUserLogin", username)
+                    .queryParam("Password", password)
+                    .build();
+            urlString = uri.toUriString();
+            sessionID = restTemplate.postForObject(urlString, null, OtrsSession.class);
+            ROLE_AGENT = false;
+        }
+
+        if (sessionID.getSessionId()==null) {
+            throw new BadCredentialsException("Incorrect username or password.");
+        }
+        else{
+            String url = "http://it.nvrs.net:7777/otrs/nph-genericinterface.pl/Webservice/GenericTicketConnectorREST/Session/" + sessionID.getSessionId();
+            OtrsUserInfo userInfo = restTemplate.getForObject(url, OtrsUserInfo.class);
+           /* for (int i = 0; i<userInfo.sessionDataSize(); i++)
+               System.out.println(userInfo.getsessionData(i)); - Вывод массива SessionData*/
+
+        }
+
+        //TODO callSESSION
+
+      /*  String userURL = "http://it.nvrs.net:7777/otrs/nph-genericinterface.pl/Webservice/GenericTicketConnectorREST/Session/" + sessionID.getSessionId();
+        System.out.println(userURL);*/
+
+       UriComponents tic = UriComponentsBuilder
         		.fromHttpUrl("http://it.nvrs.net:7777/otrs/nph-genericinterface.pl/Webservice/GenericTicketConnectorREST/Ticket")
         		.queryParam("UserLogin", username)
                 .queryParam("Password", password)
         		.build();
         		
-        String urlString = uri.toUriString();
+
         String urlTicket = tic.toUriString();
-		System.out.println(urlString);
-		System.out.println(urlTicket);
-		RestTemplate restTemplate = new RestTemplate();
-		
-		OtrsSession result = restTemplate.postForObject(urlString, null, OtrsSession.class);
+		System.out.println(sessionID.getSessionId());
 		OtrsTickets tickets = restTemplate.getForObject(urlTicket, OtrsTickets.class);
-		System.out.println(result.getSessionId());
-		
-   /*     if (!("1".equals(username) && "1".equals(password))) {
-            throw new BadCredentialsException("Incorrect username or password.");
-        }
-*/for (int i = 0; i<tickets.ticketNumber(); i++)
-	System.out.println(tickets.getTicket(i));
 
         Collection<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_CUSTOMER"));
 
+        if(ROLE_AGENT == true){
+        authorities.add(new SimpleGrantedAuthority("ROLE_AGENT"));}
+        else
+        {
+            authorities.add(new SimpleGrantedAuthority("ROLE_CUSTOMER"));
+        }
         return new UsernamePasswordAuthenticationToken(username, password, authorities);
     }
 
