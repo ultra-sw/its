bddfe3b995c8baccf76cb59a0b3eb725e438444c
Timur <wow.timur@icloud.com>
2017-11-17 01:10:56 +0300

    checkpoint

---
+++ src/main/java/ru/ultrasoftware/its/bean/SessionID.java
@@ -0,0 +1,7 @@
+package ru.ultrasoftware.its.bean;
+
+/**
+ * Created by pwmn on 16/11/2017.
+ */
+public class SessionID {
+}
---
+++ src/main/java/ru/ultrasoftware/its/bean/SessionID_Config.java
@@ -0,0 +1,13 @@
+package ru.ultrasoftware.its.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by pwmn on 16/11/2017.
+ */
+@Configuration
+public class SessionID_Config {
+    @Bean
+ public SessionID
+}
